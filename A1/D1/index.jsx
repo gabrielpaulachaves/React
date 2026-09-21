8f6d459cb6a6c29017dde5d0118ebe11c089e13d
Gabriel <bielpchaves@gmail.com>
2026-09-21 11:51:45 -0300

    tentando desafio

---
+++ A1/D1/index.jsx
@@ -0,0 +1,4 @@
+import { StrictMode } from 'react'; //o proprio react
+import { createRoot } from 'react-dom/client'; //biblioteca react que "conversa" com o navegador
+
+import app from "ttt.js"
---
+++ A1/D1/ttt.jsx
@@ -0,0 +1,20 @@
+export default function Square() {
+        //esse default diz que é a funcao principal do seu arquivo para outros arquivos que estão utilizando
+  return  <>
+  <div className="board">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+  </div>
+  <div className="board">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+  </div>
+  <div className="board">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+  </div>
+  </> 
+}
