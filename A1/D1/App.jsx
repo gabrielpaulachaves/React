f0c7b31cf9649d3b486b06c3964d8b6601404c3a
Gabriel <bielpchaves@gmail.com>
2026-09-22 09:24:03 -0300

    w

--- A1/D1/App.jsx
+++ A1/D1/App.jsx
@@ -1,5 +1,5 @@
-export default function Square() {
-        //esse default diz que é a funcao principal do seu arquivo para outros arquivos que estão utilizando
+export default function Board() {
+        //esse default diz que é a funcao principal do seu arquivo para outros arquivos que estão utilizando. Tbm da liberdade para as outras pastas que importam dar o nome que quiser
   return  <>
   <div className="board">
       <button className="square">X</button>
--- A1/D1/index.jsx
+++ A1/D1/index.jsx
@@ -1,4 +1,11 @@
 import { StrictMode } from 'react'; //o proprio react
 import { createRoot } from 'react-dom/client'; //biblioteca react que "conversa" com o navegador
 
-import app from "ttt.js"
+import App from "./App"
+
+const root = createRoot(document.getElementById("root"))  //é basicamente a conexao do react com o HTML. O react vai controlar tudo do HTML que estiver dentro desa root
+root.render((
+    <StrictMode>
+        <App />
+    </StrictMode>
+))
